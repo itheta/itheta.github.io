c1b3ca04b9f2784999f421b854b40ef9fb7b3086
aritrabiswas <aritra.biswas.stats@gmail.com>
2018-04-11 20:09:37 +0530

    r script for blog

---
+++ script.R
@@ -0,0 +1,4 @@
+library(blogdown)
+new_site() # default theme is lithium
+# need to stop serving so can use the console again
+install_theme("eddiewebb/hugo-resume", theme_example = TRUE, update_config = TRUE)
